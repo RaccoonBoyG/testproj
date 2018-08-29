06950ca8ed3a67adfb8c16693bfcd7875a7489ed
anonimD08 <asdebug@yandex.ru>
2018-08-29 17:22:19 +0500

    temp

--- testproj/testproj/settings.py
+++ testproj/testproj/settings.py
@@ -67,7 +67,7 @@ TEMPLATES_CONTEXT_PROCESSORS = [
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
