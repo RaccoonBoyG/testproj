fa091447ae1203a11084c77e56ca313979d44ec9
anonimD08 <asdebug@yandex.ru>
2018-08-30 12:52:00 +0500

    testlog

--- testproj/monitor/views.py
+++ testproj/monitor/views.py
@@ -31,7 +31,7 @@ def upload_from_json(request):
     logRDD = sc.textFile("/home/alex/big_data_edx/tracking.log")
     test = logRDD.filter(lambda line: "username" in line)
     test = normlizejson(test.first())
-    logger.info("TEST: ",test)
+    logger.info(test)
     test2 = logRDD.count()
     context = {
         'first_obj': test,
