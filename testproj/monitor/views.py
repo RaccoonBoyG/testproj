873738233ffc1826be8fd35101abcc008ec8e2a4
anonimD08 <asdebug@yandex.ru>
2018-08-30 11:33:58 +0500

    test#2

--- testproj/monitor/views.py
+++ testproj/monitor/views.py
@@ -11,8 +11,10 @@ def upload_from_json(request):
     conf = SparkConf().setAppName('TestProjApp')
     sc = SparkContext.getOrCreate(conf=conf)
     logRDD = sc.textFile("/home/alex/big_data_edx/tracking.log")
-    test = logRDD.count()
+    test = logRDD.take(5)
+    test2 = logRDD.count()
     context = {
         'first_obj': test,
+        'second_obj':test2,
     }
     return render(request, 'upload_from_json.html', context)
