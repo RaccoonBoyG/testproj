62da84681aea2b398a84e0978ed5916e52b85607
anonimD08 <asdebug@yandex.ru>
2018-08-30 12:49:21 +0500

    testJson

--- testproj/monitor/views.py
+++ testproj/monitor/views.py
@@ -4,16 +4,33 @@ from django.shortcuts import render, redirect
 
 from pyspark import SparkContext, SparkConf
 import logging
-
+import json
 
 logger = logging.getLogger('cel_logging')
+
+def normlizejson(log):
+    words = []
+    stufflist = list()
+    if(type(log) == list()):
+        for line in log:
+            j = json.loads(line)
+            words = j
+            stufflist.append(words)
+        return stufflist
+    elif(type(log) == str()):
+        j = json.loads(log)
+        words = j
+        stufflist.append(words)
+        return stufflist
+
+
 
 def upload_from_json(request):
     conf = SparkConf().setAppName('TestProjApp')
     sc = SparkContext.getOrCreate(conf=conf)
     logRDD = sc.textFile("/home/alex/big_data_edx/tracking.log")
     test = logRDD.filter(lambda line: "username" in line)
-    test.first()
+    test = normlizejson(test.first())
     test2 = logRDD.count()
     context = {
         'first_obj': test,
