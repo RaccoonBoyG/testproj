fbe3c7c609b826a5a7bfa6e4995b8abd281b47bf
anonimD08 <asdebug@yandex.ru>
2018-08-30 11:40:13 +0500

    test#4

--- testproj/monitor/views.py
+++ testproj/monitor/views.py
@@ -5,13 +5,15 @@ from django.shortcuts import render, redirect
 from pyspark import SparkContext, SparkConf
 import logging
 
+
 logger = logging.getLogger('cel_logging')
 
 def upload_from_json(request):
     conf = SparkConf().setAppName('TestProjApp')
     sc = SparkContext.getOrCreate(conf=conf)
     logRDD = sc.textFile("/home/alex/big_data_edx/tracking.log")
-    test = logRDD.take(5)
+    test = logRDD.filter(lambda line: "username" in line)
+    test.take(5)
     test2 = logRDD.count()
     context = {
         'first_obj': test,
