d658dc972e53d188c91bcfd7853cd7c817d5d7bc
anonimD08 <asdebug@yandex.ru>
2018-09-10 19:36:51 +0500

    base.html

--- testproj/monitor/views.py
+++ testproj/monitor/views.py
@@ -5,40 +5,96 @@ import logging
 #import pandas as pd
 
 from pyspark import SparkContext, SparkConf
-#from pyspark.sql import SQLContext
+from pyspark.sql import SQLContext
+import pyspark.sql.functions as F
+from datetime import datetime
+
 
 logger = logging.getLogger('cel_logging')
 
-def normlizejson(log):
-    words = []
-    stufflist = list()
-    if(type(log) is list):
-        for line in log:
-            j = json.loads(line)
-            words = j
-            stufflist.append(words)
-        return stufflist
-    elif(type(log) is str):
-        j = json.loads(log)
-        words = j
-        stufflist.append(words)
-        return stufflist
+def el_in_line(line, els):
+    b = []
+    for el in els:
+        b.append(el in line)
+    return not any(b)
+
+def filter_log(line):  
+        return el_in_line(line, ['/container/','Drupal','/instructor','{"username": ""','/info','edx.ui.lms.link_clicked','/jump_to','/progress','seek_video','play_video','pause_video','load_video','/xblock/','/xmodule/','edx.ui.lms.sequence.next_selected','stop_video','seq_goto','seq_next','problem_graded','speed_change_video','problem_check','/course/','edx.ui.lms.sequence.previous_selected','/masquerade'])
+
+def filter_csv(line):
+    if(("unenrolled") not in line):
+        return line
+
+def calculateTime(df_log):
+    time_open = {}
+    user_obj = {}
+    time_all = []
+    for row in df_log.take(50):
+        if row.page==None:
+            time_last_open = row.time.split('.',1)[0]
+            time_last_open1 = datetime.strptime(time_last_open, "%Y-%m-%dT%H:%M:%S")
+            buf.append(row.username)
+            time_open[row.event_type]=time_last_open
+    for item in buf:
+        if item==item:
+            print(item)
+    time_all.append(user_obj)
+    return time_all
+
+        elif row.page!=None:
+            time_last_close = row.time.split('.',1)[0]
+            time_last_close1 = datetime.strptime(time_last_close, "%Y-%m-%dT%H:%M:%S")
+            time_result[row.event_type]=time_last_close1
+            time_result[row.username]='page_close'
+            time_all.append(time_result)
+
+
+df_time = calculateTime(df_log_test)
+print(df_time)
+
+def workDataFrame(df_log,df_csv):
+    df_log = df_log[['username','time','event_type','page']]
+    #df_csv = df_csv[['_c2','_c0','_c1','_c3','_c4','_c5','_c6','_c7','_c8']]
+    #test_join = df_csv.join(df_log, df_csv._c2 == df_log.username, how='full')
+    test_join = test_join.filter(test_join.username.isNotNull()).distinct()
+    #test_join = test_join.filter((test_join._c2=="125507") & (test_join.course_id!=""))
+    return test_join
+def timeNextPage(test_start):
+    if(test_start[['event_type']]=="page_close"):
+        test_start = test_start.withColumn('next_time_page', F.lead(test_start['time']))
+        return test_start
 
 
 def upload_from_json(request):
-    conf = SparkConf().setAppName('TestProjApp')
-    sc = SparkContext.getOrCreate(conf=conf)
-    #sql_sc = SQLContext(sc)
-    csvRDD = sc.textFile('/home/alex/big_data_edx/track/UrFU_IHA.b.Hu-0063.1_spring_2018_problem_grade_report_2018-08-30-1000.csv').map(lambda line: line.split(","))
-    #s_df = sql_sc.createDataFrame(pandas_df)
-    logRDD = sc.textFile("/home/alex/big_data_edx/track/tracking.log")
-    csv = csvRDD.collect()
-    log = logRDD.filter(lambda line: "alexKekovich" in line)
-    log = normlizejson(log.first())
-    countLog = logRDD.count()
+conf = SparkConf().setAppName('TestProjApp')
+sc = SparkContext.getOrCreate(conf=conf)
+sql_sc = SQLContext(sc)
+logRDD = sc.textFile("/home/alex/big_data_edx/track/TPU+IN+2017_05.log.gz")
+logRDD = logRDD.map(lambda line: line.split('{', 1)[1])
+char_elem = '{'
+logRDD = logRDD.map(lambda line: f'{char_elem}{line}')
+log = logRDD.filter(filter_log)
+df_log = sql_sc.read.json(log)
+df_log = df_log[['username','time','event_type','page']]
+new_column = F.when(df_log.event_type!='page_close', F.split('event_type','/')[5]).when(df_log.event_type=='page_close',F.split('page','/')[7]).otherwise('page_close')
+df_log_test = df_log.withColumn('event_type', new_column)
+df_log_test = df_log_test.filter(df_log_test.event_type != '')
+#new_column1 = F.when(df_log.event_type=='page_close',F.split('page','/')[7])
+
+csvRDD = sc.textFile('/home/alex/big_data_edx/track/TPU_IN_2017_05_grade_report_2018-08-31-1106.csv')
+csv = csvRDD.filter(filter_csv)
+df_csv = sql_sc.read.csv(csv)
+result_df = workDataFrame(df_log,df_csv).persist()
+test_start = result_df[['username','time','event_type']]
+test_start = test_start.withColumn('next_time', F.lead(test_start['time']).over(Window.partitionBy("username").orderBy('time')))
+timeFmt = "yyyy-MM-dd'T'HH:mm:ss"
+timeDiff = (F.unix_timestamp('time', format=timeFmt)-F.unix_timestamp('prev_time', format=timeFmt))
+test_start = test_start.withColumn("Duration", timeDiff)
+
+    #log = normlizejson(log.first())
+    #countLog = logRDD.count()
     context = {
         'csv': csv,
         'log': log,
-        'countLog':countLog,
     }
     return render(request, 'upload_from_json.html', context)
