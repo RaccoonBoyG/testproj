c473b0bffee8b0daeac438996e15cf2d1ae8e5b1
anonimD08 <asdebug@yandex.ru>
2018-09-11 14:24:55 +0500

    test

--- testproj/monitor/models.py
+++ testproj/monitor/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 
 class Document(models.Model):
-    description = models.CharField(max_length=255, blank=True)
+    description = models.CharField(max_length=255, blank=True, null=True)
     document = models.FileField(upload_to='uploads/')
     uploaded_at = models.DateTimeField(auto_now_add=True)
