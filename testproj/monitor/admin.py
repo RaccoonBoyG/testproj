66398a64b1a95b028b1a71a88e90c6454a945a76
anonimD08 <asdebug@yandex.ru>
2018-09-11 14:42:55 +0500

    test

--- testproj/monitor/admin.py
+++ testproj/monitor/admin.py
@@ -5,6 +5,6 @@ from .models import Document
 @admin.register(Document)
 class DocumentAdmin(admin.ModelAdmin):
     search_fields = ("document", "uploaded_at")
-    list_display = ("document", "uploaded_at")
+    list_display = ("description","document", "uploaded_at")
 
 
