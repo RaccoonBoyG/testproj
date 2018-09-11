dbea0d7d599c894fe335a3b292405a0de8900505
anonimD08 <asdebug@yandex.ru>
2018-09-11 18:27:36 +0500

    test js

--- testproj/static/js/progress_bar.js
+++ testproj/static/js/progress_bar.js
@@ -1,22 +1,25 @@
-$(function() {
-
-    var bar = $('.bar');
-    var percent = $('.percent');
-    var status = $('#status');
-
-    console.log("TEST")
-
-    $('form').ajaxForm({
-        beforeSend: function() {
-            status.empty();
-            var percentVal = '0%';
-            bar.width(percentVal);
-            percent.html(percentVal);
-        },
-        uploadProgress: function(event, position, total, percentComplete) {
-            var percentVal = percentComplete + '%';
-            bar.width(percentVal);
-            percent.html(percentVal);
-        }
+$( function() {
+    var progressbar = $( "#progressbar" ),
+    progressLabel = $( “.progress-label” );
+    
+    progressbar.progressbar({
+    value: false,
+    change: function() {
+    progressLabel.text( progressbar.progressbar( “value” ) + “%” );
+    },
+    complete: function() {
+    progressLabel.text( “Complete!” );
+    }
     });
-}); 
+    function progress() {
+    var val = progressbar.progressbar( “value” ) || 0;
+    
+    progressbar.progressbar( “value”, val + 2 );
+    
+    if ( val < 99 ) {
+    setTimeout( progress, 80 );
+    }
+    }
+    
+    setTimeout( progress, 2000 );
+    } );
