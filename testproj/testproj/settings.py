d3dffa349a188f1124e1ab2479dab3eef4c746b8
anonimD08 <asdebug@yandex.ru>
2018-09-11 10:48:39 +0500

    test6

--- testproj/testproj/settings.py
+++ testproj/testproj/settings.py
@@ -171,10 +171,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"), ]
-STATICFILES_FINDERS = [
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-]
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"), ]
+#STATICFILES_FINDERS = [
+#    'django.contrib.staticfiles.finders.FileSystemFinder',
+#    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+#]
 MEDIA_ROOT = os.path.join(BASE_DIR, "uploads")
 MEDIA_URL = '/uploads/'
