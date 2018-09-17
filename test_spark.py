fe4b0038d15daad65b299421809fadd30369141a
anonimD08 <asdebug@yandex.ru>
2018-09-17 15:36:11 +0500

    test stream rdd convert

--- test_spark.py
+++ test_spark.py
@@ -35,6 +35,15 @@ def upload_from_spark(sc,sql_sc):
     mydict = df_log_test1.toPandas().set_index('id').T.to_dict('list')
     pickle.dump(mydict, open("/tmp/mydict", "wb"))
 
+def filter_convert_rdd(rddRaw):
+    rdd = rddRaw.map(lambda line: str(line))
+    rdd = rdd.map(lambda line: line.split('{', 1)[1])
+    char_elem = '{'
+    rdd = rdd.map(lambda line: f'{char_elem}{line}')
+    log = rdd.filter(filter_log)
+    df_log = sql_sc.read.json(log).persist()
+    df_log.show()
+
 if __name__ == "__main__":
     #sc = SparkSession.builder.master("local").appName("TestProjApp").getOrCreate()
     conf = SparkConf().setAppName('TestProjApp')
@@ -44,18 +53,9 @@ if __name__ == "__main__":
     sql_sc = SQLContext(sc)
     ssc.checkpoint("/tmp/spark")
     logRDD = ssc.textFileStream("testproj/uploads/uploads/*.gz")
-    logRDD = logRDD.map(lambda line: line.split('{', 1)[1])
-    char_elem = '{'
-    logRDD = logRDD.map(lambda line: f'{char_elem}{line}')
-    log = logRDD.filter(filter_log)
-    df_log = sql_sc.read.json(log).persist()
-    df_log = df_log[['username','time','event_type','page']]
-    new_column = F.when(df_log.event_type!='page_close', F.split('event_type','/')[5]).when(df_log.event_type=='page_close',F.split('page','/')[7]).otherwise('page_close')
-    df_log_test = df_log.withColumn('event_type', new_column)
-    df_log_test = df_log_test.filter(df_log_test.event_type != '')
-    df_log_test1 = df_log_test.withColumn("id",F.monotonically_increasing_id())
     
-    df_log_test1.show()
+    logRDD.foreachRDD(lambda rddRaw: filter_convert_rdd(rddRaw))
+    
     ssc.start()
     ssc.awaitTermination()
     #mydict = df_log_test1.toPandas().set_index('id').T.to_dict('list')
