2ed087dfa6f31d728e08144ba92ad04c937e9865
anonimD08 <asdebug@yandex.ru>
2018-09-17 11:04:27 +0500

    test_spark5

--- test_spark.py
+++ test_spark.py
@@ -35,6 +35,6 @@ def upload_from_spark(sc,sql_sc):
     pickle.dump(mydict, open("/tmp/mydict", "wb"))
 
 if __name__ == "__main__":
-    sc = SparkSession.builder.setMaster(“local”).setAppName("TestProjApp").getOrCreate()
+    sc = SparkSession.builder.setMaster("local").setAppName("TestProjApp").getOrCreate()
     #conf = SparkConf().setAppName('TestProjApp')
     #sc = SparkContext.getOrCreate(conf=conf)
