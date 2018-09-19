33aa974451e2b89331bde317f2783553d8defeb8
anonimD08 <asdebug@yandex.ru>
2018-09-19 12:47:25 +0500

    test2

--- testproj/testproj/celery.py
+++ testproj/testproj/celery.py
@@ -4,7 +4,7 @@ from celery import Celery
 from django.conf import settings
 
 # set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testproj.monitor')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testproj.settings')
 
 app = Celery('django.conf:settings')
 
