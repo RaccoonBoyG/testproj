177620a7a7432457080e770871d583cb02264de9
anonimD08 <asdebug@yandex.ru>
2018-09-19 17:54:53 +0500

    test

--- testproj/monitor/admin.py
+++ testproj/monitor/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Document
+from .models import Document, DataSet
 
 
 @admin.register(Document)
@@ -7,4 +7,8 @@ class DocumentAdmin(admin.ModelAdmin):
     search_fields = ("document", "uploaded_at")
     list_display = ("description","document", "uploaded_at")
 
+
+@admin.register(DataSet)
+class DataSetAdmin(admin.ModelAdmin):
+    list_display = ("spark_count")
 
