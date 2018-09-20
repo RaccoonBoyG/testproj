956dce82e586d28fe1041af812c0082d29ecc6f1
anonimD08 <asdebug@yandex.ru>
2018-09-20 09:37:43 +0500

    fix task

--- testproj/monitor/tasks.py
+++ testproj/monitor/tasks.py
@@ -2,7 +2,7 @@ import logging
 import datetime
 
 from testproj.celery import app
-from .models import Document, df_spark_count
+from .models import Document, DataSet
 
 logger = logging.getLogger('celery_logging')
 
@@ -10,4 +10,4 @@ logger = logging.getLogger('celery_logging')
 @app.task(bind=True)
 def handle_spark(self, *args):
     # logger.info("Course Начали: {0}".format(strftime("%Y-%m-%d %H:%M:%S", gmtime())))
-    df_spark_count()
+    DataSet.df_spark_count()
