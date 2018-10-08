b89798815e69b1e414c077582a6e73d679d3c4c6
anonimD08 <asdebug@yandex.ru>
2018-10-08 13:53:09 +0500

    scatter

--- testproj/monitor/views.py
+++ testproj/monitor/views.py
@@ -80,27 +80,29 @@ def page_view(request):
 
     # The `chartConfig` dict contains key-value pairs data for chart attribute
     chartConfig = OrderedDict()
-    chartConfig["caption"] = "Countries With Most Oil Reserves [2017-18]"
-    chartConfig["subCaption"] = "In MMbbl = One Million barrels"
-    chartConfig["xAxisName"] = "Country"
-    chartConfig["yAxisName"] = "Reserves (MMbbl)"
-    chartConfig["numberSuffix"] = "K"
+    chartConfig["caption"] = "Трудоёмкость курса"
+    #chartConfig["subCaption"] = "In MMbbl = One Million barrels"
+    chartConfig["xAxisName"] = "Раздел 1"
+    chartConfig["yAxisName"] = "Время(секунды)"
+    chartConfig["numberSuffix"] = "Sec"
     chartConfig["theme"] = "candy"
+    chartConfig["anchorbgcolor"] = "5D62B5"
+    chartConfig["seriesname"] = "Время"
 
     # The `chartData` dict contains key-value pairs data
     chartData = OrderedDict()
-    chartData["Venezuela"] = 290
-    chartData["Saudi"] = 260
-    chartData["Canada"] = 180
-    chartData["Iran"] = 140
-    chartData["Russia"] = 115
-    chartData["UAE"] = 100
-    chartData["US"] = 30
-    chartData["China"] = 30
+    # chartData["Venezuela"] = 290
+    # chartData["Saudi"] = 260
+    # chartData["Canada"] = 180
+    # chartData["Iran"] = 140
+    # chartData["Russia"] = 115
+    # chartData["UAE"] = 100
+    # chartData["US"] = 30
+    # chartData["China"] = 30
 
 
     dataSource["chart"] = chartConfig
-    dataSource["data"] = []
+    dataSource["data"] = "uploads/data.json"
     
     # Convert the data in the `chartData` array into a format that can be consumed by FusionCharts. 
     # The data for the chart should be in an array wherein each element of the array is a JSON object
@@ -110,12 +112,13 @@ def page_view(request):
     for key, value in chartData.items():
         data = {}
         data["label"] = key
-        data["value"] = value
+        data["y"] = value
+        data["x"] = 0
         dataSource["data"].append(data)
 
 
     # Create an object for the column 2D chart using the FusionCharts class constructor
     # The chart data is passed to the `dataSource` parameter.
-    column2D = FusionCharts("column2d", "ex1" , "600", "400", "chart-1", "json", dataSource)
+    column2D = FusionCharts("scatter", "ex1" , "1000", "800", "chart-1", "json", dataSource)
 
     return render(request, 'dashboard.html', {'output' : column2D.render(), 'chartTitle': 'Simple Chart Using Array'})
