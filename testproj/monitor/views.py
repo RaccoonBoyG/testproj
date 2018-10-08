cc4dc7b269ba71178329751dbb03a73eae4c2a64
anonimD08 <asdebug@yandex.ru>
2018-10-08 10:27:28 +0500

    test2

--- testproj/monitor/views.py
+++ testproj/monitor/views.py
@@ -13,7 +13,9 @@ from django.core.files.uploadedfile import UploadedFile
 #import pickle
 
 from .tasks import handle_spark
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
+
+from collections import OrderedDict
 from .fusioncharts import FusionCharts
 
 
@@ -74,21 +76,46 @@ def delete(request):
 
 
 def page_view(request):
-    column2d = FusionCharts("column2d", "ex1" , "600", "400", "chart-1", "json",
-        # The data is passed as a string in the `dataSource` as parameter.
-    """{  
-            "chart": {  
-                "caption":"Harry\'s SuperMart",
-                "subCaption":"Top 5 stores in last month by revenue",
-                "numberPrefix":"$",
-                "theme":"candy"
-            },
-            "data": [  
-                {"label":"Bakersfield Central", "value":"880000"},
-                {"label":"Garden Groove harbour", "value":"730000"},
-                {"label":"Los Angeles Topanga", "value":"590000"},
-                {"label":"Compton-Rancho Dom", "value":"520000"},
-                {"label":"Daly City Serramonte", "value":"330000"}
-            ]
-        }""")
-    return render(request, 'dashboard.html', {'output': column2d})
+    dataSource = OrderedDict()
+
+    # The `chartConfig` dict contains key-value pairs data for chart attribute
+    chartConfig = OrderedDict()
+    chartConfig["caption"] = "Countries With Most Oil Reserves [2017-18]"
+    chartConfig["subCaption"] = "In MMbbl = One Million barrels"
+    chartConfig["xAxisName"] = "Country"
+    chartConfig["yAxisName"] = "Reserves (MMbbl)"
+    chartConfig["numberSuffix"] = "K"
+    chartConfig["theme"] = "fusion"
+
+    # The `chartData` dict contains key-value pairs data
+    chartData = OrderedDict()
+    chartData["Venezuela"] = 290
+    chartData["Saudi"] = 260
+    chartData["Canada"] = 180
+    chartData["Iran"] = 140
+    chartData["Russia"] = 115
+    chartData["UAE"] = 100
+    chartData["US"] = 30
+    chartData["China"] = 30
+
+
+    dataSource["chart"] = chartConfig
+    dataSource["data"] = []
+    
+    # Convert the data in the `chartData` array into a format that can be consumed by FusionCharts. 
+    # The data for the chart should be in an array wherein each element of the array is a JSON object
+    # having the `label` and `value` as keys.
+
+    # Iterate through the data in `chartData` and insert in to the `dataSource['data']` list.
+    for key, value in chartData.items():
+        data = {}
+        data["label"] = key
+        data["value"] = value
+        dataSource["data"].append(data)
+
+
+    # Create an object for the column 2D chart using the FusionCharts class constructor
+    # The chart data is passed to the `dataSource` parameter.
+    column2D = FusionCharts("column2d", "ex1" , "600", "400", "chart-1", "json", dataSource)
+
+    return render(request, 'dashboard.html', {'output' : column2D.render(), 'chartTitle': 'Simple Chart Using Array'})
