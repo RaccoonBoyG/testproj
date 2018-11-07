0e66199bb7c0ee06fd20d6a046e15de82a6c5cf9
anonimD08 <asdebug@yandex.ru>
2018-11-07 12:33:22 +0500

    admin

--- testproj/monitor/admin.py
+++ testproj/monitor/admin.py
@@ -1,14 +1,38 @@
 from django.contrib import admin
-from .models import Document, DataSet
+from .models import Document, DataSet, Courses, Platform, Sections, Subsections, Pages
 
 
 @admin.register(Document)
 class DocumentAdmin(admin.ModelAdmin):
     search_fields = ("document", "uploaded_at")
-    list_display = ("description","document", "uploaded_at")
+    list_display = ("document", "uploaded_at")
 
 
 @admin.register(DataSet)
 class DataSetAdmin(admin.ModelAdmin):
     list_display = ("spark_count",)
 
+
+@admin.register(Courses)
+class CoursesAdmin(admin.ModelAdmin):
+    list_display = ("name", "session")
+
+
+@admin.register(Platform)
+class PlatformAdmin(admin.ModelAdmin):
+    list_display = ("name", "url")
+
+
+@admin.register(Sections)
+class SectionsAdmin(admin.ModelAdmin):
+    list_display = ("global_id", "name")
+
+
+@admin.register(Subsections)
+class SubsectionsAdmin(admin.ModelAdmin):
+    list_display = ("name", "section")
+
+
+@admin.register(Pages)
+class PagesAdmin(admin.ModelAdmin):
+    list_display = ("name", "subsection")
